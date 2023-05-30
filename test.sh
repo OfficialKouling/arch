d064d9af7e65ea7b60dac849a0d2cd6b076986ac
Kouling <OfficialKouling@gmail.com>
2023-05-31 01:41:27 +0200

    Add new file

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ exec sfdisk /dev/sda <<EOF
 ;
 EOF
 
-exec sfdisk --change-id /dev/sda 1 EF
+exec sfdisk --change-id /dev/sda 1 EF && mount /dev/sda1 /mnt/boot && mount /dev/sda2 /mnt && mount /dev/sda3 /mnt/home && mkdir /mnt/boot/efi
 
 exec mount /dev/sda1 /mnt/boot
 exec mount /dev/sda2 /mnt
