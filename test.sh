7cb3a69527c426691c6fe46fbe40f5d94e96da0c
Kouling <OfficialKouling@gmail.com>
2023-05-31 09:17:56 +0200

    Add new info

--- test.sh
+++ test.sh
@@ -7,9 +7,7 @@ EOF
 
 exec sfdisk --change-id /dev/sda 1 EF && mount /dev/sda1 /mnt/boot && mount /dev/sda2 /mnt && mount /dev/sda3 /mnt/home && mkdir /mnt/boot/efi
 
+exec mkdir /mnt/boot /mnt/home /mnt/boot/efi
 exec mount /dev/sda1 /mnt/boot
 exec mount /dev/sda2 /mnt
 exec mount /dev/sda3 /mnt/home
-
-exec mkdir /mnt/boot/efi
-
