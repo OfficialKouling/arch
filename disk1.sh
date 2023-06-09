f75a7a3653c106d7620aa42cc938d856a4b82d5f
Kouling <OfficialKouling@gmail.com>
2023-06-09 16:45:17 +0200

    Add new info

--- disk1.sh
+++ disk1.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-exec sfdisk /dev/sda <<EOF
+sfdisk /dev/sda <<EOF
 2048,1048576
 ,83886080
 ;
 EOF
+mkdir -p /mnt/boot /mnt/home /mnt/boot/efi && sfdisk --change-id /dev/sda 1 EF
+mkfs.fat -F 32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
+mount /dev/sda1 /mnt/boot && mount /dev/sda2 /mnt && mount /dev/sda3 /mnt/home
