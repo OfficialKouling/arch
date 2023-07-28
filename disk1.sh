031445f6b701751e02bc0200daf3ff6526b7555d
Kouling <officialkouling@gmail.com>
2023-07-28 22:05:24 +0200

    new info

--- disk1.sh
+++ disk1.sh
@@ -41,22 +41,23 @@ mount $disk4 /mnt/home
 pacstrap /mnt base base-devel linux linux-firmware vim git neofetch networkmanager
 #Configure system
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime &&
-    hwclock --systohc &&
-    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen &&
-    locale-gen &&
-    echo "LANG=en_US.UTF-8" > /etc/locale.conf &&
-    echo "${pc_name}" > /etc/hostname &&
-    echo "127.0.0.1	localhost" > /etc/hosts &&
-    echo "::1	       localhost" >> /etc/hosts &&
-    echo "127.0.0.1	${pc_name}.localdomain	${pc_name}" &&
-    systemctl enable NetworkManager &&
-    chpasswd <<<"root:${root_password}" &&
-    useradd ${username} &&
-    chpasswd <<<"${username}:${username_password}" &&
-    pacman -S grub efibootmgr &&
-    grub-install --target=x86_64-efi --efi-directory=/boot/efi &&
-    grub-mkconfig -o /boot/grub/grub.cfg &&
-    mkdir /boot/efi/EFI/boot &&
-    cp /boot/efi/EFI/arch/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
+arch-chroot /mnt <<"EOT"
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "${pc_name}" > /etc/hostname
+echo "127.0.0.1	localhost" > /etc/hosts
+echo "::1	       localhost" >> /etc/hosts
+echo "127.0.0.1	${pc_name}.localdomain	${pc_name}"
+systemctl enable NetworkManager
+chpasswd <<<"root:${root_password}"
+useradd ${username}
+chpasswd <<<"${username}:${username_password}"
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+mkdir /boot/efi/EFI/boot
+cp /boot/efi/EFI/arch/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
+EOT
