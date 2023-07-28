53d293d3de7c7c17a836dab6c1fa67b8dc1c564b
Kouling <officialkouling@gmail.com>
2023-07-28 21:39:11 +0200

    new info

--- 1disk.sh
+++ 1disk.sh
@@ -16,8 +16,9 @@ EOF
 sfdisk -n --part-type  ${disk} 2 0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
 sfdisk -n --part-type  ${disk} 3 0FC63DAF-8483-4772-8E79-3D69D8477DE4
 sfdisk -n --part-type  ${disk} 4 0FC63DAF-8483-4772-8E79-3D69D8477DE4
+sfdisk -n --part-type ${disk} 1 BC13C2FF-59E6-4262-A352-B275FD6F7172
 #Create File System
-mkdir -p /mnt/boot /mnt/home /mnt/boot/efi && sfdisk --part-type ${disk} 1 BC13C2FF-59E6-4262-A352-B275FD6F7172
+mkdir -p /mnt/boot /mnt/home /mnt/boot/efi
 mkfs.fat -F 32 $disk1
 mkswap $disk2
 swapon $disk2
