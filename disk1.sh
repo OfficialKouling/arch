0545ad17f92ac63a70e59d9a0c64001858e14bfe
Kouling <officialkouling@gmail.com>
2023-07-28 21:58:58 +0200

    new info

--- disk1.sh
+++ disk1.sh
@@ -14,24 +14,26 @@ disk1="${disk}1"
 disk2="${disk}2"
 disk3="${disk}3"
 disk4="${disk}4"
+
 #Disk partitioning
 sfdisk ${disk} <<EOF
-2048,1077247
-1077248,17854463
-17854464,80769023
+2048,1077247,b
+,16777216,S
+,62914560,L
 ;
 EOF
-sfdisk -n --change-id  ${disk} 2 82
-sfdisk -n --change-id  ${disk} 3 83
-sfdisk -n --change-id  ${disk} 4 83
+#sfdisk -n --part-type  ${disk} 2 0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+#sfdisk -n --part-type  ${disk} 3 0FC63DAF-8483-4772-8E79-3D69D8477DE4
+#sfdisk -n --part-type  ${disk} 4 0FC63DAF-8483-4772-8E79-3D69D8477DE4
+#sfdisk -n --part-type ${disk} 1 BC13C2FF-59E6-4262-A352-B275FD6F7172
 #Create File System
-mkdir -p /mnt/boot /mnt/home /mnt/boot/efi && sfdisk --change-id $disk 1 EF
+mkdir -p /mnt/boot /mnt/home /mnt/boot/efi
 mkfs.fat -F 32 $disk1
 mkswap $disk2
-swapon $disk2
 mkfs.ext4 $disk3
 mkfs.ext4 $disk4
 #Mount disks
+swapon $disk2
 mount $disk3 /mnt
 mount $disk1 /mnt/boot/efi
 mount $disk4 /mnt/home
