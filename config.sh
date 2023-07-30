2e42c9642261a7e44ad12f949f79484f953cd813
Kouling <officialkouling@gmail.com>
2023-07-30 20:34:20 +0200

    new info

--- config.sh
+++ config.sh
@@ -1,31 +1,5 @@
-sudo pacman -Syu --noconfirm
-sudo tar -xf ./cache/dwm-flexipatch.tar
-sudo mv ./dwm-flexipatch ~/
-sudo rm -Rf /etc/lightdm
-sudo mv ./cache/lightdm /etc/lightdm
-sudo pacman -Sy xorg lightdm kitty dmenu lightdm-gtk-greeter accountsservice firefox xorg-server-xephyr lightdm-slick-greeter imlib2 xorg-xinit --noconfirm
-sudo systemctl enable systemd-homed
-sudo chown root:root -R /etc/lightdm
-cd ~/dwm-flexipatch && sudo make install
-cd ~/arch/
-sudo su << "EOT"
-sed 's/twm/#twm/' /etc/X11/xinit/xinitrc > /etc/X11/xinit/a1xinitrc1
-sed 's/xclock/#xclock/' /etc/X11/xinit/a1xinitrc1 > /etc/X11/xinit/a1xinitrc2
-sed 's/exec/#exec/' /etc/X11/xinit/a1xinitrc2 > /etc/X11/xinit/a1xinitrc3
-sed 's/xterm/#xterm/'  /etc/X11/xinit/a1xinitrc3 > /etc/X11/xinit/a1xinitrc4
-echo "exec dwm" >> /etc/X11/xinit/a1xinitrc4
-sudo rm -rf /etc/X11/xinit/xinitrc
-sudo cp -r /etc/X11/xinit/a1xinitrc4 /etc/X11/xinit/xinitrc
-sudo rm -rf /etc/X11/xinit/a1xinitrc*
-exit
-EOT
-sudo systemctl enable lightdm
-sudo cp -r ./cache/script.sh ~/.
-sudo cp -r ./cache/.xprofile ~/.
-sudo mkdir ~/wallpapers
-sudo cp -r ./cache/set.jpg ~/wallpapers/set.jpg
-sudo cp -r ./cache/slick-greeter.conf /etc/lightdm/slick-greeter.conf
-sudo cp -r ./cache/background.jpg /usr/share/background.jpg
-sudo mkdir /usr/share/xsessions
-sudo cp -r ./cache/dwm.desktop /usr/share/xsessions/dwm.desktop
-sudo chown 644 /usr/share/xsessions/dwm.desktop
+#!/bin/bash
+sed 's/#greeter-session=lightdm-slick-greeter/greeter-session=lightdm-slick-greeter/' /etc/lightdm/lightdm.conf > /etc/lightdm/lightdm1.conf
+sudo rm -Rf /etc/lightdm/lightdm.conf
+sudo mv /etc/lightdm/lightdm1.conf /etc/lightdm/lightdm.conf
+
