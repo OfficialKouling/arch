8f1c957dbf0c2ecea5395d2c4e9f69d719825d01
Kouling <officialkouling@gmail.com>
2023-07-31 13:42:02 +0200

    new info

--- cache/script.sh
+++ cache/script.sh
@@ -1,2 +1,2 @@
-#!/bin/zsh
+#!/bin/bash
 /home/kouling/picom/build/src/picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffnes 500
--- pre-config.sh
+++ pre-config.sh
@@ -1,4 +1,10 @@
 sudo pacman -Syu --noconfirm
+echo "Do you have a touchpad?"
+echo "1)Yes 2)No"
+read touchpad
+echo "Which is your videocard?"
+echo "1)AMD 2)Intel HD 3)Nvidia"
+read video_card
 sudo tar -xf ./cache/dwm-flexipatch.tar
 sudo tar -xf ./cache/warpd.tar
 sudo mv ./dwm-flexipatch ~/
@@ -6,8 +12,36 @@ sudo mv ./warpd ~/.shit_from_git/
 sudo rm -Rf /etc/lightdm
 sudo mv ./cache/lightdm /etc/lightdm
 sudo pacman -Sy xorg flameshot light sxhkd lightdm kitty rofi lightdm-gtk-greeter accountsservice firefox xorg-server-xephyr lightdm-slick-greeter imlib2 xorg-xinit --noconfirm
+sudo mkdir ~/.config/rofi
+sudo cp -r ./cache/themes /usr/share/rofi/
+sudo echo '@theme "/home/kouling/.local/share/rofi/themes/spotlight-dark.rasi" ' > ~/.config/rofi/config.rasi
+sudo cp -r ./cache/fonts /usr/share/
+sudo cp -r ./cache/kitty ~/.config/
 sudo systemctl enable systemd-homed
 sudo chown root:root -R /etc/lightdm
+#Video_card
+if [ $video_card == 1 ]; then
+    sudo pacman -Sy mesa xf86-video-amdgpu vulkan-radeon --noconfirm
+    sudo cp -r ./cache/20-amdgpu.conf /etc/X11/xorg.conf.d/
+elif [ $video_card == 2  ]; then
+    sudo pacman -Sy mesa xf86-video-intel vulkan-intel --noconfirm
+    sudo cp -r ./cache/20-intel.conf /etc/X11/xorg.conf.d/
+elif [ $video_card == 3  ]; then
+    sudo pacman -Sy mesa xf86-video-nouveau nvidia-utils --noconfirm
+    sudo cp -r ./cache/20-nvidia.conf /etc/X11/xorg.conf.d
+else
+    echo "I'll install only mesa"
+    sudo pacman -Sy mesa --noconfirm
+fi
+#Touchpad
+if [ $touchpad == 1 ]; then
+    sudo pacman -Sy libinput --noconfirm
+    sudo cp -r ./cache/30-touchpad.conf /etc/X11/xorg.conf.d/
+elif [ $touchpad == 2 ]; then
+    echo "Ok"
+else
+    echo "Ok"
+fi
 cd ~/dwm-flexipatch && sudo make install
 cd ~/.shit_from_git/warpd && sudo make install
 cd ~/arch/
