7baed826e97e35107ddbe499239ed38eb91750c9
Kouling <officialkouling@gmail.com>
2023-07-31 17:29:01 +0200

    new info

--- config.sh
+++ config.sh
@@ -5,3 +5,6 @@ rm -Rf /etc/lightdm/lightdm.conf
 mv /etc/lightdm/lightdm1.conf /etc/lightdm/lightdm.conf
 EOT
 sudo pacman -Syu --noconfirm
+sudo pacman -Sy zsh curl --noconfirm
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
