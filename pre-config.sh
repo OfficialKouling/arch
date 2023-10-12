9eaa9946de42dbb24da443cce6248c8a437c3c29
Kouling <55913061+OfficialKouling@users.noreply.github.com>
2023-10-12 18:48:22 +0200

    Add files via upload
    update

--- pre-config.sh
+++ pre-config.sh
@@ -1,11 +1,22 @@
 sudo pacman -Syu --noconfirm
+echo "Select your style"
+echo "1)Blue 2)Orange 3)Skyrim"
+read theme
 echo "Do you have a touchpad?"
 echo "1)Yes 2)No"
 read touchpad
 echo "Which is your videocard?"
 echo "1)AMD 2)Intel HD 3)Nvidia"
 read video_card
-sudo tar -xf ./cache/dwm-flexipatch.tar
+if [ $theme == 2 ]; then
+    sudo tar -xf ./cache/dwm-flexipatch.tar
+elif [ $theme == 1 ]; then
+    sudo tar -xf ./cache/dwm-flexipatch1.tar
+elif [ $theme == 3 ]; then
+    sudo tar -xf ./cache/dwm-flexipatch2.tar
+else
+    sudo tar -xf ./cache/dwm-flexipatch1.tar
+fi
 sudo tar -xf ./cache/warpd.tar
 sudo tar -xf ./cache/picom.tar
 sudo tar -xf ./cache/zsh-syntax-highlighting.tar
@@ -15,31 +26,33 @@ sudo tar -xf ./cache/dwmbar.tar
 sudo mv ./dwmbar ~/.shit_from_git/
 sudo mv ./powerlevel10k ~/
 sudo mv ./.oh-my-zsh ~/
-sudo mv ./zsh-syntax-highlighting ~/.shit_from_git/
+sudo mv ./zsh-syntax-highlighting ~/.shit_from_git
 sudo mv ./dwm-flexipatch ~/
 sudo mv ./picom ~/.shit_from_git/
 sudo mv ./warpd ~/.shit_from_git/
 sudo rm -Rf /etc/lightdm
+sudo cp -r ./cache/lightdm /etc/lightdm
+sudo pacman -Sy libx11 pulseaudio libxinerama fontconfig libxft ttf-font-awesome python-pywal lsd bat thefuck cmake libev uthash libconfig feh xorg meson ninja --noconfirm
+sudo pacman -Sy python flameshot light sxhkd lightdm kitty rofi lightdm-gtk-greeter accountsservice firefox xorg-server-xephyr lightdm-slick-greeter imlib2 xorg-xinit --noconfirm
+sudo mkdir ~/.config/
+sudo mkdir ~/.config/kitty
+sudo mkdir ~/.config/rofi
+sudo cp -r ./cache/themes ~/.local/share/rofi
+sudo cp -r ./cache/fonts /usr/share/
+sudo cp -r ./cache/kitty ~/.config/kitty/
 sudo cp ./cache/molokai.vim /usr/share/vim/vim90/colors/
 sudo cp ./cache/.vimrc ~/
 sudo cp -r ./cache/.vim_runtime ~/
 sudo cp -r ./cache/.zsh-vi-mode/ ~/.shit_from_git/
-sudo cp -r ./cache/lightdm /etc/lightdm/
-sudo pacman -Sy libx11 libxinerama fontconfig libxft ttf-font-awesome python-pywal lsd bat thefuck cmake libev uthash libconfig feh xorg meson ninja --noconfirm
-sudo pacman -Sy python flameshot light sxhkd lightdm kitty rofi lightdm-gtk-greeter accountsservice firefox xorg-server-xephyr lightdm-slick-greeter imlib2 xorg-xinit --noconfirm
-sudo mkdir ~/.config/rofi
-sudo cp -r ./cache/themes ~/.local/share/rofi
-sudo cp -r ./cache/fonts /usr/share
-sudo cp -r ./cache/kitty ~/.config
 sudo systemctl enable systemd-homed
 sudo chown root:root -R /etc/lightdm
 #Video_card
 if [ $video_card == 1 ]; then
     sudo pacman -Sy mesa xf86-video-amdgpu vulkan-radeon --noconfirm
-    sudo cp -r ./cache/20-amdgpu.conf /etc/X11/xorg.conf.d
+    sudo cp -r ./cache/20-amdgpu.conf /etc/X11/xorg.conf.d/
 elif [ $video_card == 2  ]; then
     sudo pacman -Sy mesa xf86-video-intel vulkan-intel --noconfirm
-    sudo cp -r ./cache/20-intel.conf /etc/X11/xorg.conf.d
+    sudo cp -r ./cache/20-intel.conf /etc/X11/xorg.conf.d/
 elif [ $video_card == 3  ]; then
     sudo pacman -Sy mesa xf86-video-nouveau nvidia-utils --noconfirm
     sudo cp -r ./cache/20-nvidia.conf /etc/X11/xorg.conf.d
@@ -50,7 +63,7 @@ fi
 #Touchpad
 if [ $touchpad == 1 ]; then
     sudo pacman -Sy libinput --noconfirm
-    sudo cp -r ./cache/30-touchpad.conf /etc/X11/xorg.conf.d
+    sudo cp -r ./cache/30-touchpad.conf /etc/X11/xorg.conf.d/
 elif [ $touchpad == 2 ]; then
     echo "Ok"
 else
@@ -73,15 +86,23 @@ sudo rm -rf /etc/X11/xinit/a1xinitrc*
 exit
 EOT
 sudo systemctl enable lightdm
-sudo cp ./cache/script.sh ~/.
-sudo cp ./cache/.xprofile ~/.
+sudo cp -r ./cache/script.sh ~/.
+sudo cp -r ./cache/.xprofile ~/.
 sudo mkdir ~/wallpapers
-sudo cp ./cache/set.jpg ~/wallpapers/set.jpg
-sudo cp ./cache/slick-greeter.conf /etc/lightdm/slick-greeter.conf
-sudo cp ./cache/background.jpg /usr/share/background.jpg
+if [ $theme == 2 ]; then
+    sudo cp -r ./cache/set.jpg ~/wallpapers/set.jpg
+elif [ $theme == 1 ]; then
+    sudo cp -r ./cache/set1.jpg ~/wallpapers/set.jpg
+elif [ $theme == 3 ]; then
+    sudo cp -r ./cache/set2.jpg ~/wallpapers/set.jpg
+else
+    sudo cp -r ./cache/set1.jpg ~/wallpapers/set.jpg
+fi
+sudo cp -r ./cache/slick-greeter.conf /etc/lightdm/slick-greeter.conf
+sudo cp -r ./cache/background.jpg /usr/share/background.jpg
 sudo mkdir /usr/share/xsessions
-sudo cp ./cache/dwm.desktop /usr/share/xsessions/dwm.desktop
+sudo cp -r ./cache/dwm.desktop /usr/share/xsessions/dwm.desktop
 sudo chown 644 /usr/share/xsessions/dwm.desktop
-sudo cp ./cache/sxhkd.service /etc/systemd/system/sxhkd.service
+sudo cp -r ./cache/sxhkd.service /etc/systemd/system/sxhkd.service
 sudo systemctl enable sxhkd.service
-sudo cp -r ./cache/sxhkd ~/.config
+sudo cp -r ./cache/sxhkd ~/.config/
