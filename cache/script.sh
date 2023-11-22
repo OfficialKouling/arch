d5c2092fce0760c4235f3de47e88882171b4ad47
Kouling <officialkouling@gmail.com>
2023-11-22 13:39:39 +0100

    fix script

--- cache/script.sh
+++ cache/script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/.shit_from_git/picom/build/src/picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffnes 500
+~/.shit_from_git/picom/build/src/picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffnes 500 --experimental-backends
